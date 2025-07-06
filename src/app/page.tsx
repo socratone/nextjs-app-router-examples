aa58add0562de5cc656eb1f92118a0f62fb296cd
Kiwon Kim <gim2origin@gmail.com>
2025-07-07 00:05:53 +0900

    rename: unmount toast로 이름 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center h-screen">
-      <Link href="/pop-toastify">Pop Toastify</Link>
+      <Link href="/unmount-toast">Unmount Toast</Link>
     </div>
   );
 }

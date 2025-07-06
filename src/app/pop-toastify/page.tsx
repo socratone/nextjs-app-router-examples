28618dd3e0ca58f688e9d1ec79d2384a860d5541
Kiwon Kim <gim2origin@gmail.com>
2025-07-07 00:01:05 +0900

    docs: toastify 예제 페이지에 설명 추가

--- src/app/pop-toastify/page.tsx
+++ src/app/pop-toastify/page.tsx
@@ -4,6 +4,15 @@ import Link from 'next/link';
 import { useEffect } from 'react';
 import { toast } from 'react-toastify';
 
+/**
+ * 페이지를 벗어날 때 토스트가 사라지는 예제입니다.
+ *
+ * 컴포넌트가 마운트될 때, React.StrictMode의 마운트/언마운트/재마운트 사이클로 인해
+ * 토스트 메시지가 바로 사라지는 현상을 방지하기 위해 setTimeout을 사용하여 토스트 호출을 지연시킵니다.
+ *
+ * 컴포넌트가 언마운트될 때는 정리 함수가 호출되며, clearTimeout으로 타이머를 제거하고
+ * 페이지를 벗어날 때 열려있는 모든 토스트 메시지를 닫습니다.
+ */
 const Page = () => {
   useEffect(() => {
     // React.StrictMode에서 마운트/언마운트/재마운트 사이클로 인해 토스트가 바로 사라지는 현상을 방지하기 위해
